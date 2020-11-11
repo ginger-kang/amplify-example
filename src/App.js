465919b64d08a46a3e9dd073f4b752b1222199ca
ginger-kang <kdhoon07@gmail.com>
2020-11-11 14:01:11 +0900

    connect frontend to API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { createPicture } from './graphql/mutations';
+import { listPictures } from './graphql/queries';
 
-function App() {
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+const initialState = { name: '', description: '' };
+
+const App = () => {
+  const [formState, setFormState] = useState(initialState);
+  const [pictures, setPictures] = useState([]);
+
+  useEffect(() => {
+    fetchPictures();
+  }, []);
+
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value });
+  }
+
+  async function fetchPictures() {
+    try {
+      const pictureData = await API.graphql(graphqlOperation(listPictures));
+      const pictures = pictureData.data.listPictures.items;
+      setPictures(pictures);
+    } catch (err) {
+      console.log('error fetching todos');
+    }
+  }
+
+  async function addPicture() {
+    try {
+      if (!formState.name || !formState.description) return;
+      const picture = { ...formState };
+      setPictures([...pictures, picture]);
+      setFormState(initialState);
+      await API.graphql(graphqlOperation(createPicture, { input: picture }));
+    } catch (err) {
+      console.log('error creating todo:', err);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <h2>Amplify pictures</h2>
+      <input
+        onChange={(event) => setInput('name', event.target.value)}
+        style={styles.input}
+        value={formState.name}
+        placeholder="Name"
+      />
+      <input
+        onChange={(event) => setInput('description', event.target.value)}
+        style={styles.input}
+        value={formState.description}
+        placeholder="Description"
+      />
+      <button style={styles.button} onClick={addPicture}>
+        add picture
+      </button>
+      {pictures.map((picture, index) => (
+        <div key={picture.id ? picture.id : index} style={styles.picture}>
+          <p style={styles.pictureName}>{picture.name}</p>
+          <p style={styles.pictureDescription}>{picture.description}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
+const styles = {
+  container: {
+    width: 400,
+    margin: '0 auto',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  todo: { marginBottom: 15 },
+  input: {
+    border: 'none',
+    backgroundColor: '#ddd',
+    marginBottom: 10,
+    padding: 8,
+    fontSize: 18,
+  },
+  todoName: { fontSize: 20, fontWeight: 'bold' },
+  todoDescription: { marginBottom: 0 },
+  button: {
+    backgroundColor: 'black',
+    color: 'white',
+    outline: 'none',
+    fontSize: 18,
+    padding: '12px 0px',
+  },
+};
 export default App;
